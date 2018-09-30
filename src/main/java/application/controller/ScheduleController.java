d31abb35f43b62edd5fda9e74e6f33b84ed90c64
Artomov <diesel_dp@yahoo.com>
2018-09-30 22:00:05 +0300

    addgetall

--- src/main/java/application/controller/ScheduleController.java
+++ src/main/java/application/controller/ScheduleController.java
@@ -1,16 +1,26 @@
 package application.controller;
 
+import application.entity.Schedule;
+import application.service.implementations.ScheduleServise;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.List;
+
 @Controller
-@RequestMapping("/schadules")
+@RequestMapping("/schedules")
 public class ScheduleController {
-    @RequestMapping(method = RequestMethod.GET)
+
+    @Autowired
+    ScheduleServise scheduleServise;
+   // получить расписание
+    @GetMapping("/getall")
     public String getSchedule(Model model){
+        List<Schedule> scheduleList=scheduleServise.getAll();
         return "hello";
     }
 }
