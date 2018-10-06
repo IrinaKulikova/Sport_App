cba27706356dd7b01762d4f94e0febf84c15b7ba
Artomov <diesel_dp@yahoo.com>
2018-10-06 14:59:00 +0300

    add save_schedule.jsp

--- src/main/java/application/api/ScheduleAdminController.java
+++ src/main/java/application/api/ScheduleAdminController.java
@@ -1,11 +1,14 @@
 package application.api;
 
 import application.entity.Schedule;
+import application.entity.ScheduleEvent;
 import application.service.implementations.ScheduleServise;
+import application.service.implementations.SchedulesEventServise;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -15,12 +18,19 @@ import java.util.List;
 public class ScheduleAdminController {
     @Autowired
     ScheduleServise scheduleServise;
+    @Autowired
+    SchedulesEventServise schedulesEventServise;
     public String getSchedules(Model model){
         List<Schedule> scheduleList=scheduleServise.getAll();
         return "hallo";
     }
     @GetMapping("/save_schedulele_event")
-    public String getSaveSchedule_event(Model model){
+    public String getSaveScheduleEvent(){
         return "save_schedule_event";
+    }
+    @PostMapping("/save_schedulele_event")
+    public String postSaveScheduleEvent(ScheduleEvent event){
+        schedulesEventServise.save(event);
+        return "redirect:/";
     }
 }
