bb8c4f795b81233d8256eb6bc676fe1da05e6b55
Irina <cg.irenecooper@gmail.com>
2018-10-24 20:58:24 +0300

    delete training from table ajax

--- src/main/webapp/resources/js/trainings/trainings.js
+++ src/main/webapp/resources/js/trainings/trainings.js
@@ -8,9 +8,28 @@ $(function () {
             let dayid = arr[i].day.id;
             let timeid = arr[i].time.id;
             let p = document.createElement('p');
+            p.id = arr[i].id;
             p.innerHTML = arr[i].trainingType.name;
             let cell = document.getElementById(dayid + ":" + timeid);
             cell.appendChild(p);
         }
     });
+
+    $("table").dblclick(
+        function (e) {
+            alert("dbclick " + e.target.id);
+        });
+
+    $("table").contextmenu(
+        function (e) {
+            let result = confirm("Delete training?");
+            if (result === true) {
+                service.del('/api/1.0/trainings/' + e.target.children[0].id, function () {
+                    while (e.target.firstChild) {
+                        e.target.removeChild(e.target.firstChild);
+                    }
+                });
+            }
+            e.preventDefault();
+        });
 });
